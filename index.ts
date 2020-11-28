7fdac8f14c93c9d0036889162aa2ed4a93b61442
rajasekhar02 <rajavishnu123@gmail.com>
2020-11-28 08:05:25 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -1,41 +1,61 @@
-import { from, of } from "rxjs";
+import { from, merge, of } from "rxjs";
 import {
   catchError,
   concatMap,
   delay,
-  map,
   mergeMap,
-  observeOn,
   switchMap
 } from "rxjs/operators";
+import { $D } from "rxjs-debug";
 const promise = index =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (index & 1) resolve(Date.now());
-      else reject(Date.now());
+      if (index & 1) reject(index);
+      else resolve(Date.now());
     }, 1000);
   });
 const observable = from(
-  new Array(10).fill(1).map((_, index) => promise(index))
-).pipe(
-  mergeMap(x =>
-    from(x).pipe(
-      catchError(x => of("caout")),
-      map(x => <number>x * 2)
-    )
-  ),
-  concatMap(x => of(x).pipe(delay(1000)))
+  new Array(10).fill(promise(2)).map((_, index) => promise(index))
 );
-console.log("just before subscribe");
-observable.subscribe({
-  next(x) {
-    console.log("got value " + JSON.stringify(x));
-  },
-  error(err) {
-    console.error("something wrong occurred: " + err);
-  },
-  complete() {
-    console.log("done");
-  }
-});
-console.log("just after subscribe");
+//
+// Resolve Promises Parallel
+// observable
+//   .pipe(
+//     mergeMap(x =>
+//       from(x).pipe(
+//         delay(1000),
+//         catchError(x => of(`Caught Error ${Date.now()} merge`))
+//       )
+//     )
+//   )
+//   .subscribe(x => {
+//     console.log(x);
+//   });
+
+// Resolve Promises Sequentially
+// observable
+//   .pipe(
+//     concatMap(x =>
+//       from(x).pipe(
+//         delay(1000),
+//         catchError(x => of(`Caught Error ${Date.now()} concat`))
+//       )
+//     )
+//   )
+//   .subscribe(x => {
+//     console.log(x);
+//   });
+
+// Resolves only the latest Promises
+// observable
+//   .pipe(
+//     switchMap(x =>
+//       from(x).pipe(
+//         delay(1000),
+//         catchError(x => of(`Caught Error ${Date.now()} concat`))
+//       )
+//     )
+//   )
+//   .subscribe(x => {
+//     console.log(x);
+//   });
